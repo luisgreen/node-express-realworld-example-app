fffe237f2d67571cc1ff6ab6015d4ef32aec8abc
Luis Eduardo Chacón <luisgreen@gmail.com>
2020-01-27 20:09:30 -0600

    added healthcheck

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,20 @@
-var router = require('express').Router();
+var router = require("express").Router();
 
-router.use('/', require('./users'));
-router.use('/profiles', require('./profiles'));
-router.use('/articles', require('./articles'));
-router.use('/tags', require('./tags'));
+router.use("/", require("./users"));
+router.use("/profiles", require("./profiles"));
+router.use("/articles", require("./articles"));
+router.use("/tags", require("./tags"));
 
-router.use(function(err, req, res, next){
-  if(err.name === 'ValidationError'){
+router.get("/healthcheck", function(req, res) {
+  return res.status(200).json({
+      healthcheck = "OK"
+  });
+});
+
+router.use(function(err, req, res, next) {
+  if (err.name === "ValidationError") {
     return res.status(422).json({
-      errors: Object.keys(err.errors).reduce(function(errors, key){
+      errors: Object.keys(err.errors).reduce(function(errors, key) {
         errors[key] = err.errors[key].message;
 
         return errors;
